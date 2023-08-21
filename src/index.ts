3076b8d29efdff7bab07ae73021611536a90526e
Rafael Mota Alves <leafardzn@gmail.com>
2023-08-21 20:52:47 -0300

    feat: create interfaces for Github users and repositories

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface GithubUserResponse {
+  id: number;
+  login: string;
+  name: string;
+  bio: string;
+  public_repos: number;
+  repos_url: string;
+  message?: "Not Found";
+}
+
+interface GithubRepoResponse {
+  name: string;
+  description: string;
+  fork: boolean;
+  stargazers_count: number;
+}
