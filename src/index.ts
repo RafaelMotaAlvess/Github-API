7abb8921ec7de32be7c41c8d5f11701f000f769e
Rafael Mota Alves <leafardzn@gmail.com>
2023-08-21 21:13:32 -0300

    feat: function to get user info from github by username

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,25 @@ interface GithubRepoResponse {
   fork: boolean;
   stargazers_count: number;
 }
+
+const users: GithubUserResponse[] = [];
+
+async function fetchUser(username: string) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const user: GithubUserResponse = await response.json();
+
+  if (user.message) {
+    console.log("User not found!");
+  } else {
+    users.push(user);
+
+    console.log(
+      `The user ${user.login} has been saved.\n
+        \nid: ${user.id}
+        \nlogin: ${user.login}
+        \nname: ${user.name}
+        \nbio: ${user.bio}
+        \npublic repositories: ${user.public_repos}`
+    );
+  }
+}
